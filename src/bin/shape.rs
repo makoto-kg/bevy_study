4593f4e2cef34db984208506cc7773d97ddcff5f
Makoto Kogoh <neapoli.smith@gmail.com>
2022-08-04 22:52:49 +0900

    Add codes.

---
+++ src/bin/shape.rs
@@ -0,0 +1,26 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    // Rectangle
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        ..default()
+    });
+}
