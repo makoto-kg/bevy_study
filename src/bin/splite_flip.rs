dbf124de1e5d940f834361d3159516223ea40bb0
makoto-kg <neapoli.smith@gmail.com>
2022-08-06 17:25:32 +0900

    Add splite_flip.

---
+++ src/bin/splite_flip.rs
@@ -0,0 +1,22 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("branding/icon.png"),
+        sprite: Sprite {
+            flip_x: true,
+
+            flip_y: true,
+            ..default()
+        },
+        ..default()
+    });
+}
